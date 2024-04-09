7f8a8fd26db84fcec5046d276a5b77564f851467
mwangiryan12 <mwangiryan12@gmail.com>
2024-04-09 09:48:04 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -12,5 +14,15 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return ["What Does The This Keyword Mean?",
+  "What Is The Constructor OO Pattern?",
+  "Implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What Is NaN And How Can We Check For It",
+  "What Is The Difference Between StopPropagation And PreventDefault?",
+  "Immutable State And Pure Functions",
+  "What Is The Difference Between == And ===?",
+  "What Is The Difference Between Event Capturing And Bubbling?",
+  "What Is JSONP?"]
+
 }
